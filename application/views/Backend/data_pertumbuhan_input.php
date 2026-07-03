75c19994733406af06d28e2e4489840a6f6ac31c
ibrahimajifajarromadhon <ajii4262@gmail.com>
2026-07-03 13:21:09 +0700

    update bug data pertumbuhan input

--- application/views/Backend/data_pertumbuhan_input.php
+++ application/views/Backend/data_pertumbuhan_input.php
@@ -18,7 +18,7 @@
       <div class="col-md-12">
         <div class="form-group">
           <label for="balita">Nama Balita *</label>
-          <select id="balita" name="balita" class="form-control" onchange="getUsia(this.value)">
+          <select id="balita" name="balita" class="form-control">
             <option disabled selected>~Pilih Balita~</option>
             <?php
             foreach ($tbl_balita as $balita) {
@@ -29,24 +29,24 @@
           <span class="text-sm text-red-600"><?= form_error('balita') ?></span>
         </div>
       </div>
+
+      <div class="col-md-12">
+        <div class="form-group">
+          <label for="tgl_cek">Tanggal Cek Pertumbuhan *</label>
+          <input type="date" id="tgl_cek" name="tgl_cek" class="form-control">
+          <span class="text-sm text-red-600"><?= form_error('tgl_cek') ?></span>
+        </div>
+      </div>
+
       <div class="col-md-12">
         <div class="form-group">
           <label for="usia_balita">Usia (Bulan) *</label>
-          <!-- <input type="text" id="usia" name="usia" step='any' value="<?php echo set_value('usia') ?>" class="form-control" placeholder="Contoh : 1 Tahun 5 Bulan (18 Bulan)"> -->
           <input type="hidden" name="usia" class="form-control" id="usia">
           <input type="text" class="form-control" id="usia_balita" disabled>
           <span class="text-sm text-red-600"><?= form_error('usia') ?></span>
         </div>
       </div>
 
-      <div class="col-md-12">
-        <div class="form-group">
-          <label for="tgl_cek">Tanggal Cek Pertumbuhan *</label>
-          <input type="text" class="form-control" value="<?= tgl_indo(date('Y-m-d')); ?>" disabled>
-          <input type="hidden" id="tgl_cek" name="tgl_cek" value="<?= date('Y-m-d'); ?>">
-          <span class="text-sm text-red-600"><?= form_error('tgl_cek') ?></span>
-        </div>
-      </div>
 
       <div class="col-md-12">
         <div class="form-group">
@@ -91,35 +91,60 @@
 </div>
 
 <script>
+  document.addEventListener("DOMContentLoaded", function() {
 
-  function getUsia(id) {
-    var tgl_lahir = '';
-    var tgl_cek = document.getElementById('tgl_cek').value;
+    document.getElementById("balita").addEventListener("change", getUsia);
+    document.getElementById("tgl_cek").addEventListener("change", getUsia);
 
-    <?php foreach ($tbl_balita as $balita) { ?>
-      if (id == '<?php echo $balita->id_balita ?>') {
-        tgl_lahir = '<?php echo $balita->tgl_lahir ?>';
+    function getUsia() {
+
+      var id = document.getElementById("balita").value;
+      var tgl_cek = document.getElementById("tgl_cek").value;
+
+      if (id === "" || tgl_cek === "") {
+        document.getElementById("usia").value = "";
+        document.getElementById("usia_balita").value = "";
+        return;
       }
-    <?php } ?>
 
-    if (tgl_lahir !== '') {
+      var tgl_lahir = "";
+
+      <?php foreach ($tbl_balita as $balita) { ?>
+        if (id == "<?= $balita->id_balita ?>") {
+          tgl_lahir = "<?= $balita->tgl_lahir ?>";
+        }
+      <?php } ?>
+
+      if (tgl_lahir === "") {
+        return;
+      }
+
       var lahir = new Date(tgl_lahir);
       var cek = new Date(tgl_cek);
 
-      var totalBulan = (cek.getFullYear() - lahir.getFullYear()) * 12 +
+      if (isNaN(lahir.getTime()) || isNaN(cek.getTime())) {
+        return;
+      }
+
+      var totalBulan =
+        (cek.getFullYear() - lahir.getFullYear()) * 12 +
         (cek.getMonth() - lahir.getMonth());
 
-      // koreksi jika tanggal cek < tanggal lahir (dalam bulan berjalan)
       if (cek.getDate() < lahir.getDate()) {
         totalBulan--;
       }
 
-      if (totalBulan < 0) totalBulan = 0;
+      if (totalBulan < 0) {
+        totalBulan = 0;
+      }
 
       var usiaText = totalBulan + " Bulan";
 
-      document.getElementById('usia').value = usiaText;
-      document.getElementById('usia_balita').value = usiaText;
+      document.getElementById("usia").value = usiaText;
+      document.getElementById("usia_balita").value = usiaText;
     }
-  }
+
+    getUsia();
+
+  });
 </script>
